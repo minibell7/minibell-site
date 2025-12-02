9d26a21d21ea7982eac762fcb6d710587d2bf101
minibell7 <tjdudaud@gmail.com>
2025-12-03 01:30:53 +0900

    Add AdSense verification script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2875879574828967" crossOrigin="anonymous"></script>
+      </head>
       <body>
         <ThemeProvider>
           <ThemeToggle />
